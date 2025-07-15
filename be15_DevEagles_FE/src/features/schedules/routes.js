6314fa267105578ed5996f303eb867093feb7ba8
nineeko <gkxm1013@naver.com>
2025-07-15 11:59:58 +0900

    feat: 고객 예약 연동 #463

--- be15_DevEagles_FE/src/features/schedules/routes.js
+++ be15_DevEagles_FE/src/features/schedules/routes.js
@@ -1,12 +1,12 @@
 export const scheduleRoutes = [
   {
-    path: '/reserve/designer',
+    path: '/reserve/:shopId/staff',
     name: 'reserveDesigner',
     component: () => import('@/features/schedules/views/DesignerListView.vue'),
   },
 
   {
-    path: '/reserve/designer/:id',
+    path: '/reserve/:shopId/staff/:staffId',
     name: 'reserve',
     component: () => import('@/features/schedules/views/ReserveView.vue'),
   },
